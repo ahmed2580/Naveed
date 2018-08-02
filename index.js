601819267846f562333617d1d5982c544770ceb4
ahmed2580 <three-star-computer@hotmail.com>
2018-08-02 12:53:00 +0200

    my github app

---
+++ index.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('#menuBtn').click(function(e){
+      e.preventDefault();
+        $('#menu>nav').fadeToggle();
+  //       $('#social').fadeToggle(100);
+		// $('.header-text h2').fadeToggle(100);
+  //       // $('span').toggleClass('ahmed');
+  //       $('.header-text h2').toggleClass('ahmed2');
+  //       $('.header-text h3 , .header-text a  ').fadeToggle();
+        
+    });
+
+    $('#openForm').click(function(){
+    	$('.emailFormContainer').toggleClass('showEmailForm');
+    });
+    $('#closeForm').click(function(){
+    	$('.emailFormContainer').toggleClass('showEmailForm');
+    });
+});
